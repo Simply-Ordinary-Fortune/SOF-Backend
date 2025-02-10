414bb333cccd477d178e14045ad6cb0c710789cf
ose0221 <ose0221@naver.com>
2025-02-10 19:27:03 +0900

    src/app.js 주석 처리

--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const cors = require('cors'); 
-const path = require('path');
-const multer = require('multer');
-const { PrismaClient } = require('@prisma/client');
- 
-const app = express();
-const prisma = new PrismaClient(); 
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+// const multer = require('multer');
+// const { PrismaClient } = require('@prisma/client');
 
-require('dotenv').config();
-app.use(cors()); 
+// const app = express();
+// const prisma = new PrismaClient();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, 'uploads'));
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-});
-const upload = multer({ storage });
-const recordsRoutes = require('../routes/records.js');
-const statisticsRoutes = require('../records-api/routes/statistics');
+// require('dotenv').config();
+// app.use(cors());
 
-app.use('/records', recordsRoutes);
-app.use('/statistics', statisticsRoutes);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, 'uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     }
+// });
+// const upload = multer({ storage });
+// const recordsRoutes = require('../routes/records.js');
+// const statisticsRoutes = require('../records-api/routes/statistics');
 
-app.get('/', (req, res) => {
-    res.send('🚀 Server is running!');
-});
+// app.use('/records', recordsRoutes);
+// app.use('/statistics', statisticsRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).json({ error: 'API Not Found' });
-});
+// app.get('/', (req, res) => {
+//     res.send('🚀 Server is running!');
+// });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+// app.use((req, res, next) => {
+//     res.status(404).json({ error: 'API Not Found' });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`🚀 Server is running on http://localhost:${PORT}`);
+// });

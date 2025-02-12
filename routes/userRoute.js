d07a5378fbcc23a229f2f945742d9e26648547f9
ose0221 <ose0221@naver.com>
2025-02-12 16:08:55 +0900

    마이 페이지 조회 기능 추가

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, logInUser, updateNotification } from "../controllers/userController.js";
+import { createUser, logInUser, updateNotification, getMypage } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -11,5 +11,8 @@ router.get("/auth/signin", logInUser);
 
 //푸시 알림 설정
 router.patch("/notifications", updateNotification);
+
+//마이페이지 조회
+router.get("/mypage", getMypage);
 
 export default router;
